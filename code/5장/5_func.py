80811bab1dd4466c052094a9aab78156a050f4ca
chanwoo14 <scwoo0601@a.ut.ac.kr>
2026-04-06 11:23:01 +0900

    5

---
+++ code/5장/5_func.py
@@ -0,0 +1,15 @@
+# 5_func.py
+
+def return_info(name, phone, address, email):
+    contact_info = f"연락처{phone}\n이메일: {email}"
+    return f"이름: {name}\n{contact_info}\n주소: {address}"
+
+def print_info(name, phone, address, email =""):
+    contact_info = f"연락처{phone}\n이메일: {email}"
+    return (f"이름: {name}\n{contact_info}\n주소: {address}")
+
+print_info("Aaron", "010-5555-5555", "전주")
+person = return_info(
+                    email="hi@ut.ac.kr", phone="010-1111-1111",
+                     adress="교통대학교", name="Aaron")
+print(person)
